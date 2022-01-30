82adb1c1168f4e7eafb18f5ceb5044d3f7f1e36d
Jakub Šmrha <kuba.smrha@gmail.com>
2022-01-30 16:14:24 +0100

    net.in

--- src/main/java/jsmahy/ups_client/net/in/logged_in/PacketInLoggedIn.java
+++ src/main/java/jsmahy/ups_client/net/in/logged_in/PacketInLoggedIn.java
@@ -1,6 +1,14 @@
 package jsmahy.ups_client.net.in.logged_in;
 
+import jsmahy.ups_client.net.ProtocolState;
 import jsmahy.ups_client.net.in.PacketIn;
 
+/**
+ * Incoming packets in the {@link ProtocolState#LOGGED_IN} state.
+ *
+ * @author Jakub Šmrha
+ * @version 1.0
+ * @since 1.0
+ */
 public interface PacketInLoggedIn extends PacketIn {
 }
